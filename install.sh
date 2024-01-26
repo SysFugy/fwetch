2de2a035504c42174967bafbb62178a886c0f8f9
SysFugy <149673181+SysFugy@users.noreply.github.com>
2024-01-26 18:37:17 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-ghc Main.hs
-cp -r Main /bin
+ghc etc/Main.hs
+cp -r etc/Main /bin
 cp /bin/Main /bin/hfetch
-
-mkdir /etc/hfetch
-cp -r Main.hs /etc/hfetch
-cp -r GetData.hs /etc/hfetch
-cp -r logo.txt /etc/hfetch
